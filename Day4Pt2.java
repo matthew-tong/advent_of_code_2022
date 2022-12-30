66e4ca6aa3da2697ab75aa27e5de32c129c1d07e
Matt Tong <matthew.tong.2018@gmail.com>
2022-12-29 16:15:16 -0800

    Day 4 solutions

---
+++ Day4Pt2.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day4Pt2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File inputFile = new File("inputDayFour.txt");
+        Scanner input = new Scanner(inputFile);
+        int containsCount = 0;
+        while (input.hasNextLine()) {
+            String whole = input.nextLine();
+            containsCount += checkContains(whole);
+        }
+
+        System.out.println(containsCount);
+        input.close();
+    }
+
+    public static int checkContains(String whole) {
+        String part1 = whole.split(",")[0];
+        String part2 = whole.split(",")[1];
+        int low1 = Integer.parseInt(part1.split("-")[0]);
+        int high1 = Integer.parseInt(part1.split("-")[1]);
+        int low2 = Integer.parseInt(part2.split("-")[0]);
+        int high2 = Integer.parseInt(part2.split("-")[1]);
+
+        if(low1 > high2 || high1 < low2) {
+            return 0;
+         } else {
+            return 1;
+         }
+        
+    }
+}
